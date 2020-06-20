a63b8b2961c8e2062981fd9795fd4ff2135d5048
Jesus Herrera <jesusherrera1594@gmail.com>
2020-06-19 22:08:50 -0600

    cadenas 2

--- arreglos.py
+++ arreglos.py
@@ -13,6 +13,7 @@ listNumber = [4,7,5,8,45,8]
 #for
 #sintaxis: for <item en singular> in <nombre del arreglo>:
 #foreach
+
 for peakyBlinder in nombres:
     if(peakyBlinder=='Estefanía'):
         print(peakyBlinder)
@@ -20,12 +21,27 @@ for peakyBlinder in nombres:
 
 #Cadenas
 #Las cadenas(textos) son un tipo especial de areglos.
+#C: ArrayChar a = ['c','a','d','e','n','a','s'];
+#for(int i i<a.length(); i++){
+#     cout<<a[i];
+# }
+# > cadenas
+#qubit -> qubit 1|0 al mismo tiempo
 
 print('Cadenas\n')
-nombre = 'Juan Perez'
+#1024 caracteres
+#arreglo!
+nombre = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
 
 #Imprime solo J
-print(nombre[0])
+print(nombre[9])
 
+#Funciones de búsqueda
+#----------- find: devuelve la posicion en la que se encuenta la coincidencia.
+print(nombre[ nombre.find('sed') ])
 
+#Imprimir todo el texto
+print(nombre)
 
+#funciones de conversion de texto o cadenas
+print(nombre.upper())
